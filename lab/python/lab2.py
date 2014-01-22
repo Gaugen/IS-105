2f3cee6154f7573e661a59aacdec5cd1d14abec1
Gaugen <torabang@gmail.com>
2014-01-22 15:35:34 +0100

    lab2

---
+++ lab/python/lab2.py
@@ -0,0 +1,25 @@
+#IS-105 LAB2
+import sys
+#Gruppemedlemmene 
+#gruppe = { 'student1': 'Tor Arne Bang', \
+#	'student2': 'Simen Stromland', \
+#}
+
+#Oppgave 1 | Printer ut fugl naar man kalle fuksjon.
+def ascii_fugl():
+	print """ 
+                  \/_ 
+             \,   /( ,/
+              \\\' ///
+               \_ /_/
+               (./
+                '`
+				"""
+	
+# Oppgave 2 | 
+def bitAnd(x, y):
+	return x&y
+	
+test = bitAnd(6, 5)
+
+print "test: %d"
