66f524bbd1dd115f5dd09b50a5639823547b9723
simenstromland <simenstromland@gmail.com>
2014-01-24 16:58:48 +0100

    laster opp ex1-9 til github

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# prints to say its gonna count chickens
+print "i will now count my chickens"
+# print hens then calculates
+print "hens", 25 + 30 / 6
+# print roosters then calculates
+print "roosters", 100 - 25 * 3 % 4
+# prints that it will count egges
+print "now i will count the eggs"
+# calculates
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# asks if this equation is true
+print " is it true that 3 + 2 < 5 - 7?"
+# calculates
+print 3 + 2 < 5 - 7
+#asks for the answer of said equation
+print "what is 3 + 2", 3 + 2
+print "what is 5 - 7", 5 - 7
+# statement
+print "oh, thats why it is false"
+# ask if we gonna do some more equations
+print  "how about some more"
+# questsions and answers
+print "is it greater?", 5 > -2
+print "is it greater or equal?", 5 >= -2
+print "is it less or equal?", 5 <= - 2
+
+
---
+++ ex7.py
@@ -0,0 +1,22 @@
+#prints said text inside the " " 
+print "mary had a little lamb."
+print "its fleece whas white as %s" % 'snow'
+print "and everywhere that mary went"
+print '.' * 10 # whatd that do?
+#variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+#prints the variables
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
