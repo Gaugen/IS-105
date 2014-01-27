44de6489c3fde5d8712b569f3a1b055c4ef288ef
Gaugen <torabang@gmail.com>
2014-01-27 20:38:28 +0100

    jobbet med lab2

--- lab/python/lab2.py
+++ lab/python/lab2.py
@@ -16,10 +16,26 @@ def ascii_fugl():
                 '`
 				"""
 	
-# Oppgave 2 | 
+# Oppgave 2 | Finner bitAnd av x og y 
 def bitAnd(x, y):
+	x = 6
+	y = 5
 	return x&y
-	
-test = bitAnd(6, 5)
+#oppgave 3 | Finner bitAnd av x og y
+def bitAnd(x, y):
+	x = 6
+	y = 5
+	return x&y
+#Oppgave 4 | Finner bitXor av x og y
+def bitXor (x, y):
+	x = 4
+	y = 5
+	return x^y
+#Oppgave 5 | Finner bitOr av x og y
+def bitor (x, y)
+	x = 0
+	y = 0
+	return x|y
+#Oppgave 6 |
 
-print "test: %d"
+
