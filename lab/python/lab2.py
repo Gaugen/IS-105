fc5d8b5c6569718a2369cb0a68916208182827f6
Gaugen <torabang@gmail.com>
2014-01-28 10:44:08 +0100

    lekse

--- lab/python/lab2.py
+++ lab/python/lab2.py
@@ -7,35 +7,28 @@ import sys
 
 #Oppgave 1 | Printer ut fugl naar man kalle fuksjon.
 def ascii_fugl():
-	print """ 
+    print """ 
                   \/_ 
              \,   /( ,/
               \\\' ///
                \_ /_/
                (./
                 '`
-				"""
-	
-# Oppgave 2 | Finner bitAnd av x og y 
+    """
+#oppgave 2 | Finner bitAnd av x og y
 def bitAnd(x, y):
-	x = 6
-	y = 5
-	return x&y
-#oppgave 3 | Finner bitAnd av x og y
-def bitAnd(x, y):
-	x = 6
-	y = 5
-	return x&y
+    return x&y
 #Oppgave 4 | Finner bitXor av x og y
 def bitXor (x, y):
-	x = 4
-	y = 5
-	return x^y
+    return x^y
 #Oppgave 5 | Finner bitOr av x og y
-def bitor (x, y)
-	x = 0
-	y = 0
-	return x|y
-#Oppgave 6 |
+def bitOr (x, y):
+    return x|y
+#Oppgave 6 | Finner 
+def ascii8Bin (A):
+    ord('A')
+    A = '{0:08b}'.format(2)
+    return A
 
-
+ascii_fugl()
+bitAnd(6, 10)
